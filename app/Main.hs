234dcbac320f8067e70dd5eba632338fc9d68b46
Ted Satcher <ted.satcher@gmail.com>
2021-11-28 11:12:02 -0600

    Rendering board using original logic.

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,6 @@ import Brick.Widgets.Core
     (
      (<=>)
     , hBox
-    , vLimit
     , withBorderStyle
     , str
     , txt
@@ -30,11 +29,36 @@ rowHeader =
     let rows = fmap (T.pack . show) ([1..10] :: [Int])
     in T.intercalate "\n" rows
 
+renderBoard :: T.Text
+renderBoard =
+    let boardCoords =
+            [(x,y) | x <- ([0..9] :: [Int]), y <- ([0..9] :: [Int])]
+        markers = T.pack $ fmap boardMarker boardCoords
+        markers' = T.intersperse ' ' markers
+        rows' = [columnHeader] <> markersToRows markers' [] 0
+
+        -- Just a constant for the time being
+        boardMarker _ = '_'
+
+        -- |A recursive function to split the single row of markers
+        -- |into a list of rows appending the row number to each row.
+        markersToRows :: T.Text -> [T.Text] -> Int -> [T.Text]
+        -- Base case, we're done.
+        markersToRows "" rows _ = reverse rows
+        markersToRows ms rows n =
+            let (r,ms') = T.splitAt 20 ms
+                pad = if n < 9 then "  " else " "
+                rowNum = T.append (T.pack $ show (n+1)) pad
+            in markersToRows ms' (rowNum <> r:rows) (n+1)
+
+    in T.intercalate "\n" rows'
+
+
 borderBox :: Widget ()
 borderBox =
     withBorderStyle BS.unicode $
     B.borderWithLabel (str " Player 1 ") $
-    txt $ T.concat [columnHeader, "\n", rowHeader]
+    txt $ renderBoard
 
 ui :: Widget ()
 ui = C.center $ hBox [borderBox]
