585a33d275394d2427cc46757234046d738422ca
Ted Satcher <ted.satcher@gmail.com>
2021-11-28 10:09:30 -0600

    It's a start.  Drawing a box with a title.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+--import qualified Graphics.Vty as V
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Main as M
+import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Border as B
+
+import Brick.Types
+    ( Widget
+    )
+
+import Brick.Widgets.Core
+    (
+     vLimit
+    , withBorderStyle
+    , str
+    , txt
+    )
+
+
+ui :: Widget ()
+ui =
+    withBorderStyle BS.unicode $
+    B.borderWithLabel (str " Player 1 ") $
+    vLimit 10 $
+    C.vCenter $
+    txt $ " Ship Board "
+
 
 main :: IO ()
-main = someFunc
+main = M.simpleMain ui
